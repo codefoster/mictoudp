d9f3977321e11ad4d0fce2e10c3abe25e8d6baaf
Jeremy Foster <jeremy.foster@live.com>
2019-10-16 21:40:29 -0700

    save recorded byte stream as wav file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 var dgram = require('dgram');
 var server = dgram.createSocket("udp4");
 let port = process.argv[2] || 8089;
+import { FileWriter } from "wav";
 
-server.on('listening', function () {
-  let { address, port } = server.address();
-  console.log('UDP Server started and listening on ' + address + ":" + port);
+let outputFileStream = new FileWriter(`./${Date.now()}.wav`, {
+  sampleRate: 44100,
+  channels: 2
 });
-
-server.on("message", msg => console.log(msg));
-
+server.on('listening', () => console.log(`listening on ${port}`));
+server.on("message", msg => outputFileStream.write(msg));
 server.bind(port);
