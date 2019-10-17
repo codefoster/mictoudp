d42f643e85e1efb124a56642f3585e2fd8b1016b
Jeremy Foster <jeremy.foster@live.com>
2019-10-16 21:59:42 -0700

    write a new file every 15 seconds

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 var dgram = require('dgram');
-var server = dgram.createSocket("udp4");
-let port = process.argv[2] || 8089;
+var socket = dgram.createSocket("udp4");
+let port = process.argv[2] || 8089; //npm start <port#>
 import { FileWriter } from "wav";
 
-let outputFileStream = new FileWriter(`./${Date.now()}.wav`, {
-  sampleRate: 44100,
-  channels: 2
-});
-server.on('listening', () => console.log(`listening on ${port}`));
-server.on("message", msg => outputFileStream.write(msg));
-server.bind(port);
+let createFileWriter = () => new FileWriter(`./${Date.now()}.wav`, { sampleRate: 44100, channels: 2 });
+let output = createFileWriter();
+
+socket.on('listening', () => console.log(`listening on ${port}`));
+
+// when a byte is received, write it to the output
+socket.on("message", msg => output.write(msg));
+
+// every 15 seconds
+setInterval(() => {
+  // stop the last one
+  output.end();
+
+  // and start a new one
+  output = createFileWriter();
+}, 15000);
+
+socket.bind(port);
